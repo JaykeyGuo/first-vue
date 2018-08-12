f495fe0cf5c84323395a8906cbd9ee94bdbd5417
Jaykey.Guo <jkland927@gmail.com>
2018-08-12 13:18:54 +0800

    use vue-cli create task

--- task-vue/src/App.vue
+++ task-vue/src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mt-5">
+    <div class="row justify-content-md-center">
+    <TaskMenu v-on:click-select="onSelected" v-bind:items="menus" />
+    <TaskList  v-bind:tag="currentMenuStatus" v-on:click-complete="onCompleted" v-bind:items="tasks" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskMenu from './components/TaskMenu.vue';
+import TaskList from './components/TaskList.vue';
+import axios from 'axios';
+import * as R from 'ramda';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TaskMenu,
+    TaskList
+  },
+  data() {
+    return {
+      menus: [
+        {
+          tag: false,
+          text: '未完成'
+        },
+        {
+          tag: true,
+          text: '完成'
+        }
+      ],
+      currentMenuStatus: false,
+      tasks: [] // [{id, title, content, completed, createAt, completeAt }]Î
+    };
+  },
+  beforeCreate() {
+    axios
+      .get('https://api.myjson.com/bins/oufit')
+      .then(response => response.data)
+      .then(data => (this.tasks = data))
+      .catch(console.log);
+  },
+  methods: {
+    onSelected(tag) {
+      this.currentMenuStatus = tag;
+    },
+    onCompleted(id) {
+      var index = R.findIndex(R.propEq('id', id))(this.tasks);
+      this.tasks[index].completed = true;
+      this.save();
+    },
+    save() {
+      axios
+        .put('https://api.myjson.com/bins/oufit', this.tasks)
+        .then(response => response.data)
+        .then(data => alert('更新成功'))
+        .catch(console.log);
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
