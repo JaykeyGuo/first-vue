7e922a6a7db42c80b3d90c94d8c04cfbd459f973
Jaykey.Guo <jkland927@gmail.com>
2018-08-16 21:09:59 +0800

    use Mint UI in project

--- my-project/src/views/About.vue
+++ my-project/src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <mt-range
+      v-model="rangeValue"
+      :min="10"
+      :max="90"
+      :step="10"
+      :bar-height="5">
+    </mt-range>
   </div>
 </template>
--- my-project/src/views/HelloVue.vue
+++ my-project/src/views/HelloVue.vue
@@ -2,5 +2,9 @@
 <template>
   <div class="about">
     <h1>Hello Vue</h1>
+    <mt-spinner color="#26a2ff"></mt-spinner>
+    <mt-button type="default">default</mt-button>
+    <mt-button type="primary">primary</mt-button>
+    <mt-button type="danger">danger</mt-button>
   </div>
 </template>
