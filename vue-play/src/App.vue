db417a3e8367792bfd5ad1696c47af44a23b480f
Jaykey.Guo <jkland927@gmail.com>
2019-12-04 11:11:43 +0800

    add basic config for vue-play

--- vue-play/src/App.vue
+++ vue-play/src/App.vue
@@ -2,13 +2,16 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/form">Form</router-link> |
+      <router-link to="/otp">OTP</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import '@/styles/public/reset.scss';
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
